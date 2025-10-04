12dfb50b5846b34d4c8495ddec0bceb5fad8b041
Mariyasultan00 <mariyasultan460@gmail.com>
2025-10-04 18:08:28 +0530

    Configure for GitHub Pages deployment with environment-based API URLs

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-chat',
@@ -18,10 +19,16 @@ export class ChatComponent {
   sendMessage() {
     if (!this.message) return;
     this.chatLog.push("You: " + this.message);
-    this.http.post<any>('http://127.0.0.1:8000/api/chat/', { message: this.message })
-      .subscribe(res => {
-        this.chatLog.push(res.reply);
-        this.message = '';
+    this.http.post<any>(`${environment.apiUrl}/chat/`, { message: this.message })
+      .subscribe({
+        next: (res) => {
+          this.chatLog.push(res.reply);
+          this.message = '';
+        },
+        error: (err) => {
+          this.chatLog.push("Error: " + err.message);
+          this.message = '';
+        }
       });
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-backend-url.herokuapp.com/api'  // Update this with your actual backend URL
+};

7fc96825a334ce97b30a3659d4c127b9f5565144
Mariyasultan00 <mariyasultan460@gmail.com>
2025-10-05 17:19:11 +0530

    Cleared repo

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { ChatComponent } from './app/components/chat/chat.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-bootstrapApplication(ChatComponent)
-  .catch(err => console.error(err));
